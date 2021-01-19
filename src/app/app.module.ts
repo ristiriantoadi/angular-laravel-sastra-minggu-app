338373e46d9e39bbc64f7e5be3e2757a4cbd30ec
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-01-20 06:40:28 +0800

    add admin,guest, and user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuestComponent } from './guest/guest.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuestComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
