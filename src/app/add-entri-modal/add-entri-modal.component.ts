cc84778df7b2729caa086b7a6ae3f6554aa6c536
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-01-30 06:14:08 +0800

    Update add-entri-modal.component.ts

--- src/app/add-entri-modal/add-entri-modal.component.ts
+++ src/app/add-entri-modal/add-entri-modal.component.ts
@@ -11,20 +11,15 @@ import { LaporanPemuatanService } from '../laporan-pemuatan.service';
   styleUrls: ['./add-entri-modal.component.css']
 })
 export class AddEntriModalComponent implements OnInit {
-
   fileBuktiPemuatan:File;
-  
   idPengarang=0
-
   errorFileBukanGambar=false;
   errorJudulKosong=false;
   errorNamaPengarangKosong=false;
   errorMediaKosong = false;
   errorTanggalMuatKosong = false;
   errorBuktiPemuatanKosong = false;
-
   pengarangs = []
-  
   dataEntri:IEntri={
     judul: null,
     namaPengarang:null,
@@ -34,7 +29,6 @@ export class AddEntriModalComponent implements OnInit {
     buktiPemuatan:null,
     idPengarang:null
   };
-  
   addEntriForm = this.formBuilder.group({
     judul: null,
     namaPengarang:null,
@@ -46,12 +40,10 @@ export class AddEntriModalComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,public activeModal:NgbActiveModal, private laporanPemuatanService:LaporanPemuatanService) {}
 
-
   ngOnInit(): void {
   }
 
   onSubmit(){
-
     //reset error message
     this.errorBuktiPemuatanKosong=false;
     this.errorFileBukanGambar=false;
@@ -59,7 +51,6 @@ export class AddEntriModalComponent implements OnInit {
     this.errorMediaKosong=false;
     this.errorNamaPengarangKosong=false;
     this.errorTanggalMuatKosong=false;
-
     //cek apakah form tidak lengkap
     if(this.addEntriForm.value.judul == '' || this.addEntriForm.value.namaPengarang == '' 
     || this.addEntriForm.value.media == '' || this.addEntriForm.value.tanggalMuat == ''
@@ -68,32 +59,25 @@ export class AddEntriModalComponent implements OnInit {
       if(this.addEntriForm.value.judul == '' || this.addEntriForm.value.judul == null){
         this.errorJudulKosong = true;
       }
-
       if(this.addEntriForm.value.namaPengarang == '' || this.addEntriForm.value.namaPengarang == null){
         this.errorNamaPengarangKosong = true;
       }
-      
       if(this.addEntriForm.value.media == '' || this.addEntriForm.value.media == null){
         this.errorMediaKosong = true;
       }
-
       if(this.addEntriForm.value.tanggalMuat == '' || this.addEntriForm.value.tanggalMuat == null){
         this.errorTanggalMuatKosong = true;
       }
-
       if(this.fileBuktiPemuatan == null){
         this.errorBuktiPemuatanKosong = true;
       }
-
       return;
     }
-
     //cek apakah file adalah gambar
     if(!this.isFileGambar()){
       this.errorFileBukanGambar=true;
       return;
     }
-    
     this.dataEntri.judul = this.addEntriForm.value.judul;
     this.dataEntri.namaPengarang = this.addEntriForm.value.namaPengarang;
     this.dataEntri.media = this.addEntriForm.value.media;
@@ -101,10 +85,9 @@ export class AddEntriModalComponent implements OnInit {
     this.dataEntri.jenisKarya = this.addEntriForm.value.jenisKarya;
     this.dataEntri.buktiPemuatan = this.fileBuktiPemuatan;
     this.dataEntri.idPengarang = this.idPengarang;
-
     this.laporanPemuatanService.addEntri(this.dataEntri,this);
   }
-
+  
   isFileGambar(){
     var format=this.fileBuktiPemuatan.name.split(".")[this.fileBuktiPemuatan.name.split(".").length-1]
     if(format == "jpg" || format == "png" || format == "jpeg"){
@@ -131,10 +114,8 @@ export class AddEntriModalComponent implements OnInit {
   }
 
   namaPengarangChosen(){
-    
     //get the namaPengarang value from form
     var namaPengarangChosen = this.addEntriForm.value.namaPengarang
-
     //search through the pengarangs data to see what pengarang match
     var pengarangMatch=false
     this.pengarangs.forEach((pengarang)=>{
@@ -146,7 +127,5 @@ export class AddEntriModalComponent implements OnInit {
     if(pengarangMatch==false){
       this.idPengarang=0
     }
-    
   }
-
 }
